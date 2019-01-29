cbc124d389d4657fccc8f3c5497e863e164d8d71
13100 <1310072293@qq.com>
2019-01-29 22:18:39 +0800

    一个论坛项目的测试部分

--- dubbo-service1/src/main/java/com/annie/service/SetNameImpl.java
+++ dubbo-service1/src/main/java/com/annie/service/SetNameImpl.java
@@ -1,17 +1,13 @@
 package com.annie.service;
 
-
 import com.alibaba.dubbo.config.annotation.Service;
 import com.annie.inteface.SetName;
 import org.springframework.stereotype.Component;
-
 import java.util.UUID;
 
 @Service(version = "1.0.0", timeout = 10000, interfaceClass = SetName.class)
 @Component
 public class SetNameImpl implements SetName {
-
-
     @Override
     public String setName(String name) {
         String uuid = String.valueOf(UUID.randomUUID());
--- dubbo-service2/src/main/java/com/annie/service/GetNameImpl.java
+++ dubbo-service2/src/main/java/com/annie/service/GetNameImpl.java
@@ -5,11 +5,12 @@ import com.annie.inteface.GetName;
 import org.springframework.stereotype.Component;
 
 
-@Service(version = "1.0.1", timeout = 10000, interfaceClass = GetName.class)
+@Service(version = "1.0.0", timeout = 10000, interfaceClass = GetName.class)
 @Component
 public class GetNameImpl implements GetName {
     @Override
     public String getName(String name) {
+
         return "你的名字是：" + name;
     }
 }
