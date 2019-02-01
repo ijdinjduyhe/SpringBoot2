304513a8032e7cd07a9ca9bc0aa64013246edd06
13100 <1310072293@qq.com>
2019-02-01 14:12:46 +0800

    一个论坛项目的测试部分

---
+++ dubbo-web/src/main/java/com/annie/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.annie.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestController {
+
+
+
+}
