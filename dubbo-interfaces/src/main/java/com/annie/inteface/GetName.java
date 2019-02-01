076fb39a6e4340ce3c54a190065c8d15e7caed91
13100 <1310072293@qq.com>
2019-02-01 19:08:37 +0800

    一个论坛项目的测试部分

--- dubbo-interfaces/src/main/java/com/annie/inteface/GetName.java
+++ dubbo-interfaces/src/main/java/com/annie/inteface/GetName.java
@@ -1,9 +1,5 @@
 package com.annie.inteface;
 
 public interface GetName {
-
     public String getName(String name);
-
-
-
 }
--- mybatis/src/main/java/com/annie/controller/TestController.java
+++ mybatis/src/main/java/com/annie/controller/TestController.java
@@ -1,31 +1,27 @@
 package com.annie.controller;
 
-import com.annie.service.TestService;
-import com.github.pagehelper.PageInfo;
+
+import com.annie.entity.User;
+import com.annie.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
+import java.util.List;
 
 @Controller
-public class Testontroller {
+public class TestController {
 
     @Autowired
-    TestService testService;
+    private UserMapper userMapper;
 
 
+    @RequestMapping("/test")
     @ResponseBody
-    @RequestMapping("/getName")
-    public PageInfo getName(){
-        PageInfo name = testService.getName();
-        return name;
+    public List<User> getName(){
+        List<User> users = userMapper.selectByExample(null);
+        return users;
     }
-
-
-
-
-
 
 }
