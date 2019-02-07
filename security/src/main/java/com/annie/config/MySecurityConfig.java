b94a946e8dd9b9aca30391ec4b44cf3c9ed5157a
13100 <1310072293@qq.com>
2019-02-07 23:39:10 +0800

    SpringBoot2 各个技术整合

---
+++ security/src/main/java/com/annie/config/MySecurityConfig.java
@@ -0,0 +1,10 @@
+package com.annie.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+}
