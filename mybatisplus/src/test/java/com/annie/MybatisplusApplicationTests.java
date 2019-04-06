2dc86d8236d2c9e3aeed3b7ce351acaa4719e6c0
13100 <1310072293@qq.com>
2019-04-06 20:23:35 +0800

    添加mybatis plus

---
+++ mybatisplus/src/test/java/com/annie/MybatisplusApplicationTests.java
@@ -0,0 +1,20 @@
+package com.annie;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+public class MybatisplusApplicationTests {
+
+
+
+}
