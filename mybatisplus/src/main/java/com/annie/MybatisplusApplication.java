fd2242c99dccf0e759d99bc33ea6790c86610cd0
13100 <1310072293@qq.com>
2019-04-06 22:26:43 +0800

    添加mybatis plus

--- mybatisplus/src/main/java/com/annie/MybatisplusApplication.java
+++ mybatisplus/src/main/java/com/annie/MybatisplusApplication.java
@@ -1,13 +1,28 @@
 package com.annie;
 
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@MapperScan("com.annie.dao")
 public class MybatisplusApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MybatisplusApplication.class, args);
     }
+
+
+    /**
+     * mybatis-plus分页插件<br>     * 文档：http://mp.baomidou.com<br>
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
+
 
 }
